b6e8790ec2ee88ae8d30660e9ec01a3f99a49448
Neyan najat <nyannajat8@gmail.com>
2018-09-19 13:16:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
-function getIssues() {
-}
 
-function showIssues(json) {
-}
-
-function createIssue() {
-}
-
-function showResults(json) {
-}
-
-function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
-}
-
-function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  return ''
-}
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>JavaScript fetch() Lab</title>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+    crossorigin="anonymous"></script>
+    <style>
+      .flexbox-container {
+        display: -ms-flex;
+        display: -webkit-flex;
+        display: flex;
+      }
+      .flexbox-container > div {
+        width: 50%;
+        padding: 10px;
+      }
+      .flexbox-container > div:first-child {
+        margin-right: 20px;
+      }
+    </style>
+  </head>
+  <body>
+    <main id="main">
+      <div id="errors"></div>
+    </main>
+    <div class="flexbox-container">
+      <div>
+        <h3>Create Fork</h3>
+        <p><a href="#" onclick="forkRepo()">Fork Repository</a></p>
+        <div id="results"></div>
+      </div>
+      <div>
+        <h3>Issues</h3>
+        <div>
+          Issue Title: <input type="text" id="title"><br>
+          Issue Text: <input type="text" id="body"><br>
+          <a href="#" id="search" onclick="createIssue()">Create Issue</a>
+        </div>
+        <div id="issues"></div>
+      </div>
+    </div>
+    <script src="index.js"></script>
+  </body>
+</html>
